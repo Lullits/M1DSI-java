0e0cc2bf31e7f0d5f9f7fc13d2d18fd6f8272990
aluno <aluno@DSN1251230724>
2022-04-11 10:40:33 -0300

    novopjt

---
+++ OrientcaoAoObjeto/src/Calculadora.java
@@ -0,0 +1,30 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		double n1 = 5;
+		double n2 = 2;
+		double rsom;
+		double rsub;
+		double rmul;
+		double rdv;
+		
+		
+		
+		rsom = n1 + n2;
+		System.out.println("O resultado da soma é: " + rsom);
+
+		rsub = n1 - n2;
+		System.out.println("O resultado da subtração é: " + rsub);
+		
+		rmul = n1 * n2;
+		System.out.println("O resultado da multiplicação é: " + rmul);
+		
+		rdv = n1 / n2;
+		System.out.println("O resultado da divisão é: " + rdv);
+		
+		
+
+	}
+
+}
