4cfc6c7fdd68856dfab4a9296263b93ba3a7888e
aluno <aluno@DSN1251230724>
2022-05-02 11:32:41 -0300

    incrementar

---
+++ ElementosEstaticos/src/Aplicacao.java
@@ -0,0 +1,38 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+//  Matematica matematica = new Matematica();
+//	int resul = matematica.somar(2, 3);
+//	System.out.println(resul);
+//	resul = matematica.subtrair(10,4);
+//	System.out.println(resul);			
+// quando os metodos são estaticos eles são chamados diretamente não precisa de todas as linhas
+// utilizamos os metodos estaticos quando ele so tiver a ver com a classe, sem nenhum atributo ou obj.
+
+		
+//	int resul = Matematica.somar(6, 4);
+//	System.out.println(resul);
+//	resul = Matematica.subtrair(9, 4);
+//	System.out.println(resul);		
+				
+//		Contador contador1 = new Contador(); 
+//		Contador contador2 = new Contador();
+//		Contador contador3 = new Contador();
+//		
+//		contador1.incrementar();
+//		
+//		System.out.println(contador1.getValor());
+//		System.out.println(contador2.getValor());
+//		System.out.println(contador3.getValor());
+
+//		Contador.incrementar();
+//		Contador.incrementar();
+//		
+//		System.out.println(Contador.getValor());
+
+		System.out.println(Constantes.MEDIA_DA_PROVA);
+	}
+
+}
---
+++ ElementosEstaticos/src/Matematica.java
@@ -0,0 +1,17 @@
+
+public class Matematica {
+
+	public static int somar (int a, int b ) {
+		return a+b;
+	}
+	
+	public static int subtrair(int a, int b) {
+		return a-b;
+	}
+	
+	// quando os metodos são estaticos eles são chamados diretamente
+	
+	
+	
+	
+}
