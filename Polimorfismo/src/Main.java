4f926c4a2efa6d204768e80e98c706f9a5397332
aluno <aluno@DSN1251017044>
2022-05-04 11:36:16 -0300

    Polimorfismo anotações

---
+++ Polimorfismo/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Animal animal = new Cachorro();
+		
+			animal.falar();
+		
+		animal = new Gato();
+				animal.falar();
+		
+		
+	}
+
+}
+//instanciar= criar uma variavel "Animal 'animal'<-Variavel" se add = new se transforma em construtor e depois de new é um objeto.
+//variavel guarda objetos e classes se herdar do mesmo tema. (se for extended ele pode ser utilizado como um objeto) 
+//polimorfismo= criação de um objeto generico aberto, e ele vai ter a forma/ endereço guardado dentro dele e dele pode ter diferentes escolhas a partir dela
+//nos auxilia em evitar criar varios códigos, variaveis e obejtos, faz o teste pra saber qual a opção e realiza a ação desejada em cada um. (no final terão de fazer a mesma coisa.)
