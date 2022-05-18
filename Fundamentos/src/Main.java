9c2bd2ffb3d42eba0ac56249a980ace937773142
aluno <aluno@DSN1251017057>
2022-05-18 09:02:08 -0300

    array e array Bidimensional

--- Fundamentos/src/Main.java
+++ Fundamentos/src/Main.java
@@ -151,37 +151,78 @@ public class Main {
 //	}
 // }
 //		array
-		int x1 = 3;
-		int y1 = 5;
-		int z1 = 7;
+//		int x1 = 3;
+//		int y1 = 5;
+//		int z1 = 7;
+//		
+//		System.out.println(x1);
+//		System.out.println(y1);
+//		System.out.println(z1);
+//		System.out.println();
+//		
+//		//estabelece os valores direto
+//		double x[] = {3, 5, 3, 5};
+//		
+//		for (int aux = 0; aux <x.length; aux++) { //aux= vari qualquer  se inicou por 0 por causa do inicio da matriz (0 ao infinito), .lenth=atributo conta quantas casinhas tem mesmo se aumentar não da nenhum problema, pois conta todos disponiveis
+//			System.out.println("Valor do índice" + aux + "=" + x[aux]); //mostra o resultado
+//		}
+//		
+//		System.out.println();
+//		String nome[] = {"Luana", "Esmeralda", "Aurora"}; // matriz de string, só se recebe string mesmo sendo object
+//		
+//		for (int aux1 = 0; aux1 < nome.length; aux1++) { 
+//			System.out.println(nome[aux1]); // mostra o que foi dado
+//			if (nome[aux1] == "Esmeralda") { // caso queira procurar um dado da matriz
+//				System.out.println("O índice de Esmeralda é = " + aux1); // o resultado da matriz
+//			}
+//		}
+//		
+//		
+//		int[] arrayInt = new int [5];
+//		String[] arrayString =  new String [4];
+//		double [] arrayDouble = new double [3];
+//		
+//		arrayInt[3] = 4;
+//		
+//		int[] numero = new int [];
+//		Scanner teclado = new Scanner (System.in);
+//		
+//		for (int aux2 = 0 aux2 < numero.length; aux2++) {
+//			System.out.println("Digite um número qualquer");
+//			numero[aux2] = teclado.nextInt();
+//		}
+//		
+//		for (int aux2 = 0; aux2 <numero.length; aux2++) {
+//			System.out.println("Indice" + aux2 + "=" + numero[aux2]);
+//		}
 		
-		System.out.println(x1);
-		System.out.println(y1);
-		System.out.println(z1);
-		System.out.println();
+//
+//		int []arrayQlq = new int [3];
+//		Object [] arrayObj =  new Object[5];
+//		
+//		arrayObj[0] = 12;
+//		arrayObj[0] = "Luiza";
+//		arrayObj[0] = 13.0;
+//		arrayObj[0] = true;
+//		arrayObj[0] = arrayQlq;
+//		
+//		for (int aux2 = 0; aux2 < arrayObj.length; aux2++) {
+//			System.out.println("Indice" + aux2 + "=" + arrayObj[aux2]);
+//		}
 		
-		double x[] = {3, 5, 3, 5};
+		int [][] arrayBidirecional = new int [4] [3];
 		
-		for (int aux = 0; aux <x.length; aux++) {
-			System.out.println("Valor do índice" + aux + "=" + x[aux]);
-		}
+		arrayBidirecional[0][0]=9;
+		arrayBidirecional[2][1]=7;
 		
-		System.out.println();
-		String nome[] = {"Luana", "Esmeralda", "Aurora"};
-		
-		for (int aux1 = 0; aux1 < nome.length; aux1++) {
-			System.out.println(nome[aux1]);
-			if (nome[aux1] == "Esmeralda") {
-				System.out.println("O índice de Esmeralda é = " + aux1);
+		for (int linha = 0; linha < arrayBidirecional.length; linha++) {
+			for (int coluna = 0; coluna < arrayBidirecional[0].length; coluna++) {
+				System.out.print("Indice[" +linha+ "][" +coluna+ "] = "+ arrayBidirecional[linha][coluna]+"    ");
 			}
+			System.out.println();
 		}
 		
-		
-		
-		
-		
-		
-		
+				
 		
 	}
 }	
