5bec360a1ee35e4364e32fe1a26a5eef99847448
Lullits <102962674+Lullits@users.noreply.github.com>
2022-05-22 11:58:01 -0300

    Update Aplicação.java
    main comentado

--- Sobrecarregar/src/Aplicação.java
+++ Sobrecarregar/src/Aplicação.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
 
-public class Aplicação {
+public class AplicaÃ§Ã£o {
 
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
+// "new" construtor
 
 		Math math =  new Math();
 		
 		System.out.println("Somar dois numeros:");
-		System.out.println("Digite o primeiro número");
+		System.out.println("Digite o primeiro nÃºmero");
 		int x = scanner.nextInt();
-		
-		System.out.println("Digite o segundo número");
+//pega o valor e declara para ser mostrado apenas quando o prÃ³ximo int for chamado.
+		System.out.println("Digite o segundo nÃºmero");
 		int y = scanner.nextInt();
 		
 		
 				
 		int soma = math.somar (x, y);
-		System.out.println("O resultado é " + soma + "!!!");
+//faz a soma dos dois valores
+		System.out.println("O resultado Ã© " + soma + "!!!");
+//+ concatena texto e informaÃ§Ãµes
 		
 		soma = math.somar (x,y,5);
-		System.out.println("O resultado é " + soma + "!!!");
+		System.out.println("O resultado Ã© " + soma + "!!!");
 
 		double somaa = math.somar (x,y);
-		System.out.println("O resultado é " + somaa + "!!!");
+		System.out.println("O resultado Ã© " + somaa + "!!!");
 	}
 
 }
