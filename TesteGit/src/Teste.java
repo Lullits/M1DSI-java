6313036650189221c56df6a014a2e52eaa7d171a
Lullits <102962674+Lullits@users.noreply.github.com>
2022-05-22 11:50:06 -0300

    Update Teste.java
    nome, comentado.

--- TesteGit/src/Teste.java
+++ TesteGit/src/Teste.java
@@ -2,7 +2,9 @@
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println("Luiza");
+//publico estatico vazio focado em argumentos de texto nessa ocasião.
+		System.out.println("Luiza"); 
+//SYSO utilizado entre aspas para escrever mensagens.
 	}
 
 }
