86add8ab4006830b6da93906c1351a5ebaf5e992
Lullits <102962674+Lullits@users.noreply.github.com>
2022-05-22 12:38:36 -0300

    Lampada comentada

--- Lâmpada/src/Lampada.java
+++ Lâmpada/src/Lampada.java
@@ -2,18 +2,22 @@
 public class Lampada {
 
 	private boolean Energia;
+//nomeando o atributo boolean para reconhecer o uso exato dele
 	
 	public void Lampada () {
 		Energia = true;
 		imprimir();
 	}
+//declarando que se a energia for ligada vai mostrar que ela está ligada
 	
 	public void desligar () {
 	Energia = false;
 	imprimir();
 	}
+//declarando que se a energia for desligada vai mostrar que ela está desligada
 	
 	public void imprimir () {
+//analisa se está ligada ou desligada e deixa a mensagem indicando.
 	if(Energia == true) {
 		System.out.println("ligado");
 	} else {
