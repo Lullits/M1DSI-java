50791ed54267084d7c4026e53207201f162d656c
Lullits <102962674+Lullits@users.noreply.github.com>
2022-05-22 12:40:26 -0300

    Lampada Lig/Desl Comentada

--- Lâmpada/src/main.java
+++ Lâmpada/src/main.java
@@ -4,10 +4,11 @@ public class main {
 	public static void main(String[] args) {
 
 	Lampada Lig = new Lampada();	
+//construtor da variavel privada indicando um uso que ela vai obter
 		Lig.imprimir();
 		Lig.desligar();
 		Lig.Lampada();
-
+//escreve como a lampada esta no momento.
 	}
 
 }
