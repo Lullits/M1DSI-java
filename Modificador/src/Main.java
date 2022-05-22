14f8af6b5c779b09514a79a7c93c0dad2ae5ee06
Lullits <102962674+Lullits@users.noreply.github.com>
2022-05-22 12:55:52 -0300

    Modificador main comentado

--- Modificador/src/Main.java
+++ Modificador/src/Main.java
@@ -3,34 +3,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ContaBancaria conta1 = new ContaBancaria(true, 3.0); //construtor: constrÛi obj. na memÛria heap
-//				conta1.ativo = true;
-//				conta1.numCnt = 1234;
-//				conta1.saldo = 180.50;
-//				conta1.
+		ContaBancaria conta1 = new ContaBancaria(true, 3.0); //construtor: constr√≥i obj. na mem√≥ria heap
+// Uma forma de fazer
+//	conta1.ativo = true;
+//	conta1.numCnt = 1234;
+//	conta1.saldo = 180.50;
+//	conta1.
 //				
-//				System.out.println(conta1.ativo);
-//				System.out.println(conta1.numCnt);
-//				System.out.println(conta1.saldo);
+//	System.out.println(conta1.ativo);
+//	System.out.println(conta1.numCnt);
+//	System.out.println(conta1.saldo);
 //		
-//		conta1.receber(100.8);
-//		conta1.dar(110);
-//		System.out.println(conta1.saldo());	
+//	conta1.receber(100.8);
+//	conta1.dar(110);
+//	System.out.println(conta1.saldo());	
 //		
-//		ContaBancaria conta2 = new ContaBancaria();
-//		System.out.println(conta2.saldo());
+//	ContaBancaria conta2 = new ContaBancaria();
+//	System.out.println(conta2.saldo());
 
 		Cliente cliente1 = new Cliente();
 		cliente1.setNome("Luiza");
-		cliente1.setCpf(1234567890); //chama
+		cliente1.setCpf(1234567890); //"Set" chama/declara os dados
 		cliente1.setNuCont(252525);
 		
 		System.out.println(cliente1.getNome());
-		System.out.println(cliente1.getCpf()); //recebe
+		System.out.println(cliente1.getCpf()); //recebe os dados
 		System.out.println(cliente1.getNuCont());
 
 		
-		System.out.println(cliente1.toString()); // toString mostra o endereÁo da vari·vel na heap
+		System.out.println(cliente1.toString()); // toString mostra o endere√ßo da vari√°vel na heap
 		
 		Cliente cliente2 = new Cliente();
 	
@@ -42,21 +43,21 @@ public class Main {
 		
 		//cliente1 = cliente2;
 		if (cliente1 == cliente2) {		//sem acesso ao atributo leitura apenas de cliente
-		System.out.println("Os objetos s„o iguais");
+		System.out.println("Os objetos s√£o iguais");
 		}else {
-			System.out.println("os objetos s„o diferentes");
+			System.out.println("os objetos s√£o diferentes");
 		}
 		
-		if (cliente1.getNome() == cliente2.getNome()) { // utilizado para comparaÁ„o com um aributo selecionado
-			System.out.println("Os nomes s„o iguais");
+		if (cliente1.getNome() == cliente2.getNome()) { // utilizado para compara√ß√£o com um atributo selecionado
+			System.out.println("Os nomes s√£o iguais");
 		} else {
-			System.out.println("Os nomes s„o diferentes");
+			System.out.println("Os nomes s√£o diferentes");
 		}
 		
-		if (cliente1.equals(cliente2)) { // utilizado se todos os atributos s„o iguais
-			System.out.println("Os atributos dos objetos s„o iguais");
+		if (cliente1.equals(cliente2)) { // utilizado se todos os atributos s√£o iguais
+			System.out.println("Os atributos dos objetos s√£o iguais");
 		}else {
-			System.out.println("Os atributos dos objetos s„o diferentes");
+			System.out.println("Os atributos dos objetos s√£o diferentes");
 		}
 		
 		
