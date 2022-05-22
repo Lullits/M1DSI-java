67394ab39dc65e40795a74b11a1e85dfeffa5c64
Lullits <102962674+Lullits@users.noreply.github.com>
2022-05-22 12:00:37 -0300

    Update Math.java
    comentado

--- Sobrecarregar/src/Math.java
+++ Sobrecarregar/src/Math.java
@@ -1,24 +1,23 @@
 
 public class Math {
 
-	int somar(int x, int y) {
-		
-		//int z = x + y
-		//return z; Um modo de fazer
+	int somar(int x, int y) {	
+//int z = x + y		
+//return z; Um modo de fazer
 		
 		return x + y;
 	}
-
+//soma de 2 números.
 	
 	int somar(int x, int y, int z) {
 		return x + y + z; 
 	}
-	
+//soma de 3 números
  
 	double somar (double x, double y) {
 		return x+y;
 	}
-	
+//soma de dois numeros em double (numeros com .)
 	
 	
 	
