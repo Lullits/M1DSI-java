7186212b37e31061412943b2c7b6bfbb45c19b26
aluno <aluno@DSN1251230724>
2022-05-23 11:09:11 -0300

    Arquivo txt em java

---
+++ EscreverTXT/src/main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+		String s = "texto para ser gravado no arquivo";
+		
+		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("Saida.txt"))) {	
+			escrever.write(s);			
+			
+		}
+	}
+
+}
