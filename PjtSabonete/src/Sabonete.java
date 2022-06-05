6c38713813ad88f28dd579f54d04b2f115d3a8f1
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 12:43:47 -0300

    check

--- PjtSabonete/src/Sabonete.java
+++ PjtSabonete/src/Sabonete.java
@@ -6,11 +6,12 @@ public class Sabonete {
 		
 		public Sabonete(String cor, String cheiro) { //construtor que chama a variavel direto
 			super();
+			//indicando qual a cor e cheiro 
 			this.cor = cor;
 			this.cheiro = cheiro;
 		}
 		@Override
-		public String toString() {
+		public String toString() { //como ela vai aparecer na lista
 			return "Sabonete [cor=" + cor + ", cheiro=" + cheiro + "]\n";
 		}		
 		
