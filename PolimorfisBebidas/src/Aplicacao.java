7677959e6302386ffc5227f40de81b9579f3982a
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 12:41:59 -0300

    check

--- PolimorfisBebidas/src/Aplicacao.java
+++ PolimorfisBebidas/src/Aplicacao.java
@@ -1,11 +1,11 @@
 
 public class Aplicacao {
 
-	public static void main(String[] args) { // n„o precisa criar um objeto preparador se ja È static 
+	public static void main(String[] args) { // n√£o precisa criar um objeto preparador se ja √© static 
 
-		Preparacao preparacao = new Preparacao();
+		Preparacao preparacao = new Preparacao(); //construindo um obj
 		LeiteQuente leiteQuente = new LeiteQuente();
-		preparacao.prepararBebida(leiteQuente);
+		preparacao.prepararBebida(leiteQuente);  //mostrando como se fosse o passo a passo da bebida que tem de ser preparada
 		
 		Refrigerante refrigerante = new Refrigerante();
 		preparacao.prepararBebida(refrigerante);
