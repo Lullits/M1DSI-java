4b9e3eec38c12e3ed685889d854d5477897f1221
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 13:32:17 -0300

    check

--- Interfaces/src/Automovel.java
+++ Interfaces/src/Automovel.java
@@ -1,6 +1,6 @@
 
 public interface Automovel {
-
+//algo que todos os automoveis tem em comum para evitar trabalho e diminuir a qtd de linhas.
 	void virarEsquerda();
 	void virarDireita();
 	void Acelerar();
