265c5c3df3dae3fe45d8cd3ac9bfd3ff33523ae9
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 13:37:12 -0300

    check

--- Interfaces/src/Aplicacao.java
+++ Interfaces/src/Aplicacao.java
@@ -4,21 +4,21 @@ public class Aplicacao {
 	
 	public static void main(String[] args) {
 
-		Rota rota = new Rota();
-		Ferrari ferrari = new Ferrari();
-		Fusca fusca = new Fusca();
-		rota.caminho(ferrari);
-		rota.caminho(fusca);
+		Rota rota = new Rota(); // construindo/chamando a rota
+		Ferrari ferrari = new Ferrari(); //declarando a ferrari
+		Fusca fusca = new Fusca(); //declarando o fusca
+		rota.caminho(ferrari); // a ferrari vai fazer a rota/caminho
+		rota.caminho(fusca); // o fusca tambem vai fazer a rota/caminho
 		
-		Automovel automovel = new Ferrari ();
-		rota.caminho(automovel);
-		automovel = new Fusca();
+		Automovel automovel = new Ferrari (); //dentro do automovel tem uma ferrari
+		rota.caminho(automovel); 
+		automovel = new Fusca(); // dentro do automovel tem um fusca
 		rota.caminho(automovel);
 		
-		Vendedor vendedor = new Vendedor();
-		Ape ape = new Ape();
-		vendedor.MostrarPreco(ape);
-		vendedor.MostrarPreco(ferrari);
+		Vendedor vendedor = new Vendedor(); //declarando/chamando um vendedor
+		Ape ape = new Ape(); // declarando/chamando o ape
+		vendedor.MostrarPreco(ape); // vendedor indica o valor dado ao ape
+		vendedor.MostrarPreco(ferrari); //vendedor indica o valor dado a ferrari
 
 	}
 
