bb8d0197555fa82d1d04a5eeaaac921c6bc5f9ae
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 13:25:00 -0300

    check

--- Interfaces/src/Ape.java
+++ Interfaces/src/Ape.java
@@ -1,8 +1,8 @@
 
-public class Ape implements ItemCaro {
+public class Ape implements ItemCaro { //apartamento implementa item caro
 
 	@Override
-	public double getPreco() {
+	public double getPreco() { //declarando o preço do ape (em seu val. max.)
 		return 250000;
 	}
 
