95f1c487e5fe3a945a0fd2ec60224ce5fc401fcf
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 14:33:23 -0300

    check

--- DesafioSabonete/src/Sabonete.java
+++ DesafioSabonete/src/Sabonete.java
@@ -1,16 +1,16 @@
 
-public class Sabonete {
+public class Sabonete { //declarando sabonete, cor e cheiro
 	String cor;
 	String cheiro;
 	
-	public Sabonete(String cor, String cheiro) {
+	public Sabonete(String cor, String cheiro) { //cor e cheiro strings
 		super();
 		this.cor = cor;
-		this.cheiro = cheiro;
+		this.cheiro = cheiro; //selecionando-as
 	}
 
 	@Override
-	public String toString() {
+	public String toString() { //forma que vai aparecer na tela
 		return " Sabonete [cor=" + cor + ", cheiro=" + cheiro + "]\n \n";
 	}
 
