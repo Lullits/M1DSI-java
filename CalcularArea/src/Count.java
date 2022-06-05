db5562dff4cc305aef108b91071f59748f33783b
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 14:28:25 -0300

    check

--- CalcularArea/src/Count.java
+++ CalcularArea/src/Count.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Count {
-//Ainda Não comentado
+
 	public static void main(String[] args) {
 
 		Scanner	scanner = new Scanner(System.in);
@@ -10,31 +10,31 @@ public class Count {
 
 			System.out.println("Somando �rea de um quadrado:!");
 			System.out.println("Digite a altura do quadrado");
-			int x = scanner.nextInt();
+			int x = scanner.nextInt(); //guardando a altura em x
 			
 			System.out.println("Digite a Largura do quadrado");
-			int y = scanner.nextInt();
+			int y = scanner.nextInt(); //guardando a largura em y
 
 			System.out.println("Somando �rea de um Tri�ngulo!");
 			System.out.println("Digite a Altura do Tri�ngulo");
-			int a = scanner.nextInt();
+			int a = scanner.nextInt(); //guardando a altura em a
 			
 			System.out.println("Digite a Hipotenusa do triangulo");
-			int h = scanner.nextInt();
+			int h = scanner.nextInt(); //guardando a hipotenusa em h
 			
 			System.out.println("Digite a Largura do triangulo");
-			int l = scanner.nextInt();		
+			int l = scanner.nextInt(); //guardando largura em l		
 			
 			
 			System.out.println("Somando �rea de um ret�ngulo!");
 			System.out.println("Digite a altura do ret�ngulo");
-			int t = scanner.nextInt();
+			int t = scanner.nextInt(); //guardando a altura em t
 			
 			System.out.println("Digite a Largura do ret�ngulo");
-			int g = scanner.nextInt();
+			int g = scanner.nextInt(); //guardando a largura em g
 			
 
-			
+		//fazendo as contas e mostrando todos os resultados			
 			int squa = cal.somQua (x, y);
 			System.out.println("O resultado da �rea do quadrado � " + squa + "!!!");
 			
