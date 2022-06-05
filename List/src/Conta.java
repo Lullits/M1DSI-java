fcf62a03841d742bb5cba7fd6ac2fb82fa60a803
Lullits <102962674+Lullits@users.noreply.github.com>
2022-06-05 13:08:52 -0300

    check

--- List/src/Conta.java
+++ List/src/Conta.java
@@ -4,14 +4,14 @@ public class Conta {
 	String nome;
 	double valor;
 	
-	public Conta(String nome, double valor) {
+	public Conta(String nome, double valor) { //liberando pra chamar fora dessa pag
 		super();
 		this.nome = nome;
 		this.valor = valor;
 	}
 
 	@Override
-	public String toString() {
+	public String toString() { //como vai aparecer na tela
 		return "Conta [Conta de " + nome + ",  Valor total= " + valor+ " R$]";
 		
 	}
